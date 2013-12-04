17a367af0c0a2aa3528895d0dd34ea08c7204f41
Florian Dietrich <floriandietrich@Corrigan.local>
2013-12-04 08:55:16 +0100

    Started implementation of sprites

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+$(function () {
+	/*$('.hd-low-motion-sprite').spritespin({
+		width: 1920,
+		height: 1080,
+		frames: 36,
+		resX: 1920,
+		resY: 38880,
+		source: '../images/spritespin/sprites_full_size.jpg'
+	});*/
+
+	$('.hd-low-motion-sprite').spChangeDir('down').sprite({fps: 12, no_of_frames: 36, dir: 'up'});
+});
